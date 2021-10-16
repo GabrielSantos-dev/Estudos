423c07fb0f6500d0485e5ab07c94cef0c5b7549e
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2021-10-15 23:54:51 -0300

    att

--- Frontend/Linguagem/TypeScript/Dio-TypeScript/index.ts
+++ Frontend/Linguagem/TypeScript/Dio-TypeScript/index.ts
@@ -1,3 +1,4 @@
+//Tratamento da Tag Input
 const input = document.getElementById('input') as HTMLInputElement;
 
 input.addEventListener('input', (event) => {
@@ -5,3 +6,30 @@ input.addEventListener('input', (event) => {
     console.log(i.value);
     
 });
+
+//Generic Types
+
+function adicionaApendiceALista<T>(array: T[], valor: T){
+    return array.map(()) => valor);
+}
+
+adicionaApendiceALista([1, 2 , 3], 'D');
+
+//Condicionais a partir de parâmetros
+
+interface IUsuario {
+    id: string;
+    email: string;
+}
+
+interface IAdmin {
+    cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
+
+function redirecione(usuario: IUsuario | IAdmin){
+    if ('cargo' in usuario){
+        //redirecionar para a área de administração
+    } else {
+        //redirecionar para a área do usuário
+    }
+}
