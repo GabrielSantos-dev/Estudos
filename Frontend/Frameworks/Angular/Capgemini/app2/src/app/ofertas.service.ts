646a2660a208a7058c10e094dbeceeb10dbb60bd
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2021-11-17 17:57:02 -0300

    Angular App2

---
+++ Frontend/Frameworks/Angular/Capgemini/app2/src/app/ofertas.service.ts
@@ -0,0 +1,22 @@
+import { Oferta } from './shared/oferta.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class OfertasService {
+  constructor(private http: HttpClient) {}
+
+  public getOfertas(): Promise<Oferta[]> {
+    return this.http
+      .get('http://localhost:3000/ofertas?destaque=true')
+      .toPromise()
+      .then((resposta: any) => resposta)
+  }
+
+  public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+    return this.http
+      .get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .toPromise()
+      .then((resposta: any) => resposta)
+  }
+}
