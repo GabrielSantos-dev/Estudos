2659a8c93e8aa30967544f93368dc05c168d745e
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2021-11-19 10:36:54 -0300

    Treinamento Angular

--- Frontend/Frameworks/Angular/Capgemini/app2/src/app/ofertas.service.ts
+++ Frontend/Frameworks/Angular/Capgemini/app2/src/app/ofertas.service.ts
@@ -2,21 +2,52 @@ import { Oferta } from './shared/oferta.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { URL_API } from './app.api';
+
 @Injectable()
 export class OfertasService {
+  // private url_api = 'http://localhost:3000/ofertas'
+
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Oferta[]> {
     return this.http
-      .get('http://localhost:3000/ofertas?destaque=true')
+      .get(`${URL_API}/ofertas?destaque=true`)
       .toPromise()
-      .then((resposta: any) => resposta)
+      .then((resposta: any) => resposta);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
     return this.http
-      .get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .get(`${URL_API}/ofertas?categoria=${categoria}`)
       .toPromise()
-      .then((resposta: any) => resposta)
+      .then((resposta: any) => resposta);
+  }
+
+  public getOfertaPorId(id: number): Promise<Oferta> {
+    return this.http
+      .get(`${URL_API}/ofertas?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => {
+        return resposta[0];
+      });
+  }
+
+  public getComoUsarOfertaPorId(id: number): Promise<string> {
+    return this.http
+      .get(`${URL_API}/como-usar?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => {
+        return resposta[0].descricao
+      })
+  }
+
+  public getOndeFicaOfertaPorId(id: number): Promise<string> {
+    return this.http
+      .get(`${URL_API}/onde-fica?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => {
+        return resposta[0].descricao
+      })
   }
 }
