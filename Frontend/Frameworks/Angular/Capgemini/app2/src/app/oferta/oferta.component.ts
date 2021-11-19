68f22fe0c0bc8f4cd5796aaa91f92c76b8b1840f
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2021-11-19 18:35:11 -0300

    Treinamento

--- Frontend/Frameworks/Angular/Capgemini/app2/src/app/oferta/oferta.component.ts
+++ Frontend/Frameworks/Angular/Capgemini/app2/src/app/oferta/oferta.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+// import { interval, Observable, Observer, Subscription } from 'rxjs';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
@@ -9,7 +10,10 @@ import { Oferta } from '../shared/oferta.model';
   styleUrls: ['./oferta.component.css'],
   providers: [ OfertasService ]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  // private tempoObservableSubscription!: Subscription
+  // private meuObservableTesteSubscription!: Subscription
 
   public oferta!: Oferta
 
@@ -24,11 +28,40 @@ export class OfertaComponent implements OnInit {
       this.oferta = oferta
       // console.log(this.oferta);
     })
+
+
+    // let tempo = interval(2000)
+
+    // this.tempoObservableSubscription =  tempo.subscribe((intervalo: number) => {
+    //     console.log(intervalo)
+    // })
+
+    // observable (observavel)
+    // let meuObservableTeste = new Observable((observer: Observer<number>) => {
+    //     observer.next(1)
+    //     observer.next(2)
+    //     observer.next(3)
+    //     observer.complete()
+    //     observer.next(4)
+    // })
+
+    // observable (observador)
+    // this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
+    //   (resultado: number) => console.log(resultado + 10),
+    //   (erro: string) => console.log(erro),
+    //   () => console.log('Stream de eventos foi finalizada')
+    // )
+
     // this.route.params.subscribe(
     //   (parametro: any) => { console.log(parametro) },
     //   (erro: any) => console.log(erro),
     //   () => console.log('processamento foi classificado como concluido')
     // )
+  }
+
+  ngOnDestroy() {
+    // this.meuObservableTesteSubscription.unsubscribe()
+    // this.tempoObservableSubscription.unsubscribe()
   }
 
 }
--- Frontend/Frameworks/Angular/Capgemini/app2/src/app/ofertas.service.ts
+++ Frontend/Frameworks/Angular/Capgemini/app2/src/app/ofertas.service.ts
@@ -1,6 +1,8 @@
+import { Observable } from 'rxjs';
 import { Oferta } from './shared/oferta.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, retry } from 'rxjs/operators'
 
 import { URL_API } from './app.api';
 
@@ -49,5 +51,11 @@ export class OfertasService {
       .then((resposta: any) => {
         return resposta[0].descricao
       })
+  }
+
+  public pesquisaOfertas(termo: string): Observable<Oferta[]> {
+    return this.http.get<Oferta[]>(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
+        .pipe(retry(10))
+        .pipe(map(response => response))
   }
 }
